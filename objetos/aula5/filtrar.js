174eee162679226a97234b5f0a6277cea339571d
ChristalCamillo <christal.b.camillo@gmail.com>
2023-09-15 16:29:11 -0300

    comentarios em objetos

--- objetos/aula5/filtrar.js
+++ objetos/aula5/filtrar.js
@@ -1,8 +1,14 @@
 // tentando filtrar clientes que moram em apto mas nao colocaram complemento
 
+
+// para pegar as infos do arquivo externo e salva-las em uma variavel local
 const clientes = require("./clientes.json");
 
+
+// nessa função, a lista é o parametro
 function filtrarApartamentoSemComplemento(clientes) {
+	//os arrays tem função filter para que passe por cada campo da lista, e nesse retorno trará os clientes com
+	//propriedade apartamento: true e que NÃO (!) tenha a propriedade complemento preenchida
   return clientes.filter((cliente) => {
     return (
       cliente.endereco.apartamento &&
@@ -11,6 +17,7 @@ function filtrarApartamentoSemComplemento(clientes) {
   });
 }
 
+// o retorno dessa função é então passado para uma variavel e exibido no console
 const filtrados = filtrarApartamentoSemComplemento(clientes);
 
 console.log(filtrados);
--- objetos/aula5/ordenar.js
+++ objetos/aula5/ordenar.js
@@ -3,7 +3,9 @@
 const clientes = require("./clientes.json");
 
 function ordenar(lista, propriedade) {
+	// a função sort ja ordena, temos que passar apenas como queremos a ordenação
   const resultado = lista.sort((a, b) => {
+	//essa comparação é a forma que a fun sort pede que seja feito
     if (a[propriedade] < b[propriedade]) {
       return -1;
     }
@@ -13,9 +15,15 @@ function ordenar(lista, propriedade) {
     return 0;
   });
 
+    //if (ordem !== "crescente") { forma de reverter a ordenação
+    //resultado.reverse();
+  //}
+
   return resultado;
 }
 
 const ordenadoNome = ordenar(clientes, "nome");
+
+//const ordenadoInverso = ordenadoNome.reverse(); outra forma de retornar invertido 
 
 console.log(ordenadoNome);
