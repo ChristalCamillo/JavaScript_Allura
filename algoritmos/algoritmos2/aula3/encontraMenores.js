9dcbfc059d2c691cc5e3d5ea9231635c6c2cfc96
ChristalCamillo <christal.b.camillo@gmail.com>
2023-10-09 15:52:35 -0300

    erro diferença codigo aula3

---
+++ algoritmos/algoritmos2/aula3/encontraMenores.js
@@ -0,0 +1,20 @@
+const listaLivros = require(`./array`)
+
+function encontraMenores(pivo, array) {
+  let menores = 0;
+
+  for (let atual = 0; atual < array.lenght; atual++) {
+    let produtoAtual = array[atual];
+    if (produtoAtual.preco < pivo.preco) {
+      menores++;
+    }
+  }
+
+  return menores;
+}
+
+
+
+
+
+console.trace(encontraMenores(listaLivros[2], listaLivros));
